e98bf870b3cdbcec3dc19f5504e8b862f8918cc6
Nicholas Chen <nichwch@gmail.com>
2018-10-25 10:17:57 -0700

    added clicking functionality
    clicking a square now changes it to an X

--- src/index.js
+++ src/index.js
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      value:null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square"
+      onClick={()=>{this.setState({value:'X'});}}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
